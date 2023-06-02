4e82a39616cab3e9ba4f3a1ce59bacd892937572
Garrett Hoffman <son0hoff@gmail.com>
2023-06-02 13:32:51 -0700

    work on post routes allowing one to post to the homepage community

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -15,8 +15,30 @@ cloudinary.config({
 });
 
 // Get All posts
-router.route("/").get(async (req, res) => {});
+router.route("/").get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (error) {
+    res.status(200).json({ success: false, message: error });
+  }
+});
 
 // Create a post
+router.route("/").post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
+
+    res.status(200).json({ success: true, data: newPost });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
 
 export default router;
